aad37916f76da3d7282b2e67591d91c0937a2395
WuJC <jcwu@hqu.edu.cn>
2023-05-06 16:59:22 +0800

    0506

--- src/ApproxOperator.jl
+++ src/ApproxOperator.jl
@@ -10,16 +10,32 @@ abstract type SpatialPartition end
 include("node.jl")
 include("element.jl")
 include("operation.jl")
+include("meshfree.jl")
 
-include.(filter(contains(r".jl$"), readdir("preprocession"; join=true)))
-include.(filter(contains(r".jl$"), readdir("approximation"; join=true)))
-include.(filter(contains(r".jl$"), readdir("operation"; join=true)))
+include("preprocession/integration.jl")
+include("preprocession/geometry.jl")
+include("preprocession/importcomsol.jl")
+include("approximation/quad.jl")
+include("approximation/tri3.jl")
+include("approximation/seg2.jl")
+include("approximation/poi1.jl")
+include("approximation/reproducingkernel.jl")
+include("approximation/kernelfunction.jl")
+include("approximation/rkgradientsmoothing.jl")
+include("approximation/grkgsi.jl")
+include("operation/potential.jl")
+include("operation/elasticity.jl")
+include("operation/hyperelasticity.jl")
+include("operation/error_estimates.jl")
+include("preprocession/importmsh.jl")
+include("operation/Kirchhoff_Love_plate.jl")
 
 include("littletools.jl")
 
 export prescribe!
 export Operator
 export Node, Element
+export ReproducingKernel, RKGradientSmoothing, GRKGradientSmoothing
 export set𝝭!, set∇𝝭!, set∇²𝝭!
 
 end
