9080d66480a5e27380094e372fee2555bdfcf3ac
xuytxuyt <xyt5517@163.com>
2023-05-22 14:29:55 +0800

    20230522

--- src/approximation/rkwave.jl
+++ src/approximation/rkwave.jl
@@ -1,4 +1,4 @@
 get𝑛𝒑(  ::ReproducingKernel{:Quadratic2D}) = 5
-get𝒑(   ::ReproducingKernel{:Quadratic2D},k::NTuple{2,Float64},x::NTuple{3,Float64}) = (1.,sin(k[1]x),cos(k[1]x),sin(k[2]x),cos(k[2]x))
-get∂𝒑∂x(::ReproducingKernel{:Quadratic2D},k::NTuple{2,Float64},x::NTuple{3,Float64}) = (0.,cos(k[1]x),-sin(k[1]x),0.,0.)
-get∂𝒑∂y(::ReproducingKernel{:Quadratic2D},k::NTuple{2,Float64},x::NTuple{3,Float64}) = (0.,0.,0.,cos(k[1]x),-sin(k[1]x))
+get𝒑(   ::ReproducingKernel{:Quadratic2D},x::NTuple{3,Float64}) = (1.,sin(x[1]),cos(x[1]),sin(x[2]),cos(x[2]))
+get∂𝒑∂x(::ReproducingKernel{:Quadratic2D},x::NTuple{3,Float64}) = (0.,cos(x[1]),-sin(x[1]),0.,0.)
+get∂𝒑∂y(::ReproducingKernel{:Quadratic2D},x::NTuple{3,Float64}) = (0.,0.,0.,cos(x[1]),-sin(x[1]))
