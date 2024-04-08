595803a5e700cc8a2bcc21954c41a42b46c19ec5
xuytxuyt <xyt5517@163.com>
2024-04-08 19:38:21 +0800

    20240408

--- src/operation/Mindlin_Reissner_plate.jl
+++ src/operation/Mindlin_Reissner_plate.jl
@@ -1,4 +1,4 @@
-function (op::Operator{:∫κᵢⱼγᵢⱼdΩ})(ap::T;k::AbstractMatrix{Float64}) where T<:AbstractElement
+function (op::Operator{:∫κMγQdΩ})(ap::T;k::AbstractMatrix{Float64}) where T<:AbstractElement
     𝓒 = ap.𝓒; 𝓖 = ap.𝓖
     E = op.E
     ν = op.ν
@@ -17,14 +17,67 @@ function (op::Operator{:∫κᵢⱼγᵢⱼdΩ})(ap::T;k::AbstractMatrix{Float64
             for (j,xⱼ) in enumerate(𝓒)
                 J = xⱼ.𝐼
                 k[3*I-2,3*J-2] += ( Dˢ*B₁[i]*B₁[j] + Dˢ*B₂[i]*B₂[j])*𝑤
-                k[3*I-2,3*J-1] += (-Dˢ*B₁[i]*N[j])*𝑤
+                k[3*I-2,3*J-1] += (-Dˢ*N[i]*B₁[j])*𝑤
                 k[3*I-2,3*J]   += (-Dˢ*B₂[i]*N[j])*𝑤
                 k[3*I-1,3*J-2] += (-Dˢ*N[i]*B₁[j])*𝑤
                 k[3*I-1,3*J-1] += ( Dᵇᵢᵢᵢᵢ*B₁[i]*B₁[j] + Dᵇᵢⱼᵢⱼ*B₂[i]*B₂[j] + Dˢ*N[i]*N[j])*𝑤
-                k[3*I-1,3*J]   += ( Dᵇᵢᵢⱼⱼ*B₂[i]*B₁[j])*𝑤
+                k[3*I-1,3*J]   += ( Dᵇᵢᵢⱼⱼ*B₁[i]*B₂[j] + Dᵇᵢⱼᵢⱼ*B₂[i]*B₁[j])*𝑤
                 k[3*I,3*J-2]   += (-Dˢ*N[i]*B₂[j])*𝑤
-                k[3*I,3*J-1]   += ( Dᵇᵢᵢⱼⱼ*B₁[i]*B₂[j])*𝑤
+                k[3*I,3*J-1]   += ( Dᵇᵢᵢⱼⱼ*B₂[i]*B₁[j] + Dᵇᵢⱼᵢⱼ*B₁[i]*B₂[j])*𝑤
                 k[3*I,3*J]     += ( Dᵇᵢᵢᵢᵢ*B₂[i]*B₂[j] + Dᵇᵢⱼᵢⱼ*B₁[i]*B₁[j] + Dˢ*N[i]*N[j])*𝑤
+            end
+        end
+    end
+end
+
+function (op::Operator{:∫κMdΩ})(ap::T;k::AbstractMatrix{Float64}) where T<:AbstractElement
+    𝓒 = ap.𝓒; 𝓖 = ap.𝓖
+    E = op.E
+    ν = op.ν
+    h = op.h
+    for ξ in 𝓖
+        N = ξ[:𝝭]
+        B₁ = ξ[:∂𝝭∂x]
+        B₂ = ξ[:∂𝝭∂y]
+        𝑤 = ξ.𝑤
+        Dᵇᵢᵢᵢᵢ = E*h^3/12/(1-ν^2)
+        Dᵇᵢᵢⱼⱼ = E*ν*h^3/12/(1-ν^2)
+        Dᵇᵢⱼᵢⱼ = E*h^3/24/(1+ν)
+        for (i,xᵢ) in enumerate(𝓒)
+            I = xᵢ.𝐼 
+            for (j,xⱼ) in enumerate(𝓒)
+                J = xⱼ.𝐼
+                k[3*I-1,3*J-1] += ( Dᵇᵢᵢᵢᵢ*B₁[i]*B₁[j] + Dᵇᵢⱼᵢⱼ*B₂[i]*B₂[j])*𝑤
+                k[3*I-1,3*J]   += ( Dᵇᵢᵢⱼⱼ*B₁[i]*B₂[j] + Dᵇᵢⱼᵢⱼ*B₂[i]*B₁[j])*𝑤
+                k[3*I,3*J-1]   += ( Dᵇᵢᵢⱼⱼ*B₂[i]*B₁[j] + Dᵇᵢⱼᵢⱼ*B₁[i]*B₂[j])*𝑤
+                k[3*I,3*J]     += ( Dᵇᵢᵢᵢᵢ*B₂[i]*B₂[j] + Dᵇᵢⱼᵢⱼ*B₁[i]*B₁[j])*𝑤
+            end
+        end
+    end
+end
+
+function (op::Operator{:∫γQdΩ})(ap::T;k::AbstractMatrix{Float64}) where T<:AbstractElement
+    𝓒 = ap.𝓒; 𝓖 = ap.𝓖
+    E = op.E
+    ν = op.ν
+    h = op.h
+    for ξ in 𝓖
+        N = ξ[:𝝭]
+        B₁ = ξ[:∂𝝭∂x]
+        B₂ = ξ[:∂𝝭∂y]
+        𝑤 = ξ.𝑤
+        Dˢ = 6/5*h*E/2/(1+ν)
+        for (i,xᵢ) in enumerate(𝓒)
+            I = xᵢ.𝐼 
+            for (j,xⱼ) in enumerate(𝓒)
+                J = xⱼ.𝐼
+                k[3*I-2,3*J-2] += ( Dˢ*B₁[i]*B₁[j] + Dˢ*B₂[i]*B₂[j])*𝑤
+                k[3*I-2,3*J-1] += (-Dˢ*N[i]*B₁[j])*𝑤
+                k[3*I-2,3*J]   += (-Dˢ*B₂[i]*N[j])*𝑤
+                k[3*I-1,3*J-2] += (-Dˢ*N[i]*B₁[j])*𝑤
+                k[3*I-1,3*J-1] += ( Dˢ*N[i]*N[j])*𝑤
+                k[3*I,3*J-2]   += (-Dˢ*N[i]*B₂[j])*𝑤
+                k[3*I,3*J]     += ( Dˢ*N[i]*N[j])*𝑤
             end
         end
     end
@@ -39,8 +92,6 @@ function (op::Operator{:∫wqdΩ})(ap::T;f::AbstractVector{Float64}) where T<:Ab
         for (i,xᵢ) in enumerate(𝓒)
             I = xᵢ.𝐼
             f[3*I-2] += N[i]*q*𝑤
-            f[3*I-1] += 0
-            f[3*I]   += 0
         end
     end
 end
@@ -58,8 +109,6 @@ function (op::Operator{:∫wVdΓ})(ap::T;f::AbstractVector{Float64}) where T<:Ab
         for (i,xᵢ) in enumerate(𝓒)
             I = xᵢ.𝐼
             f[3*I-2] += N[i]*V*𝑤
-            f[3*I-1] += 0
-            f[3*I]   += 0
         end
     end
 end
@@ -76,18 +125,8 @@ function (op::Operator{:∫vwdΓ})(ap::T;k::AbstractMatrix{Float64},f::AbstractV
             for (j,xⱼ) in enumerate(𝓒)
                 J = xⱼ.𝐼
                 k[3*I-2,3*J-2] += α*N[i]*N[j]*𝑤
-                k[3*I-2,3*J-1] += 0
-                k[3*I-2,3*J]   += 0
-                k[3*I-1,3*J-2] += 0
-                k[3*I-1,3*J-1] += 0
-                k[3*I-1,3*J]   += 0
-                k[3*I,3*J-2]   += 0
-                k[3*I,3*J-1]   += 0
-                k[3*I,3*J]     += 0
             end
             f[3*I-2] += α*N[i]*g*𝑤
-            f[3*I-1] += 0
-            f[3*I]   += 0
         end
     end
 end
@@ -96,30 +135,19 @@ function (op::Operator{:∫vθdΓ})(ap::T;k::AbstractMatrix{Float64},f::Abstract
     α = op.α
     for ξ in 𝓖
         𝑤 = ξ.𝑤
-        n₁ = ξ.n₁
-        n₂ = ξ.n₂
         N = ξ[:𝝭]
         θ₁ = ξ.θ₁
         θ₂ = ξ.θ₂
-        θₐ = θ₁*n₁+θ₁*n₂
-        θᵦ = θ₂*n₁+θ₂*n₂
         for (i,xᵢ) in enumerate(𝓒)
             I = xᵢ.𝐼
             for (j,xⱼ) in enumerate(𝓒)
                 J = xⱼ.𝐼
-                k[3*I-2,3*J-2] += 0
-                k[3*I-2,3*J-1] += 0
-                k[3*I-2,3*J]   += 0
-                k[3*I-1,3*J-2] += 0
                 k[3*I-1,3*J-1] += α*N[i]*N[j]*𝑤
-                k[3*I-1,3*J]   += 0
-                k[3*I,3*J-2]   += 0
-                k[3*I,3*J-1]   += 0
                 k[3*I,3*J]     += α*N[i]*N[j]*𝑤
             end
             f[3*I-2] += 0
-            f[3*I-1] += α*N[i]*θₐ*𝑤
-            f[3*I]   += α*N[i]*θᵦ*𝑤
+            f[3*I-1] += α*N[i]*θ₁*𝑤
+            f[3*I]   += α*N[i]*θ₂*𝑤
         end
     end
 end
